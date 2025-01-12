50488db8756b3411ed0137270f28878751a0c3cc
isro24 <alamat_email_anda@example.com>
2025-01-12 10:37:41 +0700

    Menambah fun deleteMhs pada HomeViewModel

--- app/src/main/java/com/example/praktikumfirebase/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikumfirebase/ui/home/viewmodel/HomeViewModel.kt
@@ -39,6 +39,15 @@ class HomeViewModel (
                 }
         }
     }
+    fun deleteMhs(mahasiswa: Mahasiswa){
+        viewModelScope.launch {
+            try {
+                repositoryMhs.deleteMhs(mahasiswa)
+            } catch (e: Exception){
+                mhsUiState = HomeuiState.Error(e)
+            }
+        }
+    }
 }
 
 sealed class HomeuiState{
