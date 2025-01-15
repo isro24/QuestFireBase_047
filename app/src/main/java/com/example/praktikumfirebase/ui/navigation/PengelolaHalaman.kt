25953a9c9d08bf8411d6916e9802044b6c2d6b9c
isro24 <alamat_email_anda@example.com>
2025-01-15 17:03:55 +0700

    UCP 3

--- app/src/main/java/com/example/praktikumfirebase/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikumfirebase/ui/navigation/PengelolaHalaman.kt
@@ -8,7 +8,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.praktikumfirebase.ui.detail.pages.MahasiswaDetailView
+//import com.example.praktikumfirebase.ui.detail.pages.MahasiswaDetailView
 import com.example.praktikumfirebase.ui.home.pages.HomeScreen
 import com.example.praktikumfirebase.ui.insert.pages.InsertMhsView
 
